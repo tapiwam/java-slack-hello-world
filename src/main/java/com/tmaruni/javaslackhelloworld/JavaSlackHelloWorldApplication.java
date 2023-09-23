84bfd9d35fc358e7e39a8ed1b4f2203652227755
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2023-09-23 18:29:34 -0500

    Blank project

---
+++ src/main/java/com/tmaruni/javaslackhelloworld/JavaSlackHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.tmaruni.javaslackhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSlackHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSlackHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmaruni/javaslackhelloworld/JavaSlackHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmaruni.javaslackhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSlackHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
