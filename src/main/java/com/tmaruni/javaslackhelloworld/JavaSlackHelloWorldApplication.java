c510c61589cc3a8d53ded1f466d8d8e55fe75609
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2023-09-23 19:17:26 -0500

    Add HelloWorld message logic.

--- src/main/java/com/tmaruni/javaslackhelloworld/JavaSlackHelloWorldApplication.java
+++ src/main/java/com/tmaruni/javaslackhelloworld/JavaSlackHelloWorldApplication.java
@@ -14,44 +14,44 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 public class JavaSlackHelloWorldApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaSlackHelloWorldApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSlackHelloWorldApplication.class, args);
+    }
 
-@Override
-public void run(String... args) {
-	log.info("EXECUTING : Sending a message to Slack!");
+    @Override
+    public void run(String... args) {
+        log.info("EXECUTING : Sending a message to Slack!");
 
-	Slack slack = Slack.getInstance();
+        Slack slack = Slack.getInstance();
 
-	// Load an env variable
-	// If the token is a bot token, it starts with `xoxb-` while if it's a user token, it starts with `xoxp-`
-	String token = System.getenv("SLACK_TOKEN");
+        // Load an env variable
+        // If the token is a bot token, it starts with `xoxb-` while if it's a user token, it starts with `xoxp-`
+        String token = System.getenv("SLACK_TOKEN");
 
-	// Initialize an API Methods client with the given token
-	MethodsClient methods = slack.methods(token);
+        // Initialize an API Methods client with the given token
+        MethodsClient methods = slack.methods(token);
 
-	// Build a request object
-	ChatPostMessageRequest request = ChatPostMessageRequest.builder()
-			.channel("#hello-world")
-			.text(":wave: Hello World!")
-			.build();
+        // Build a request object
+        ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+                .channel("#hello-world")
+                .text(":wave: Hello World!")
+                .build();
 
-	// Get a response as a Java object
-	ChatPostMessageResponse response = null;
-	try {
-		response = methods.chatPostMessage(request);
+        // Get a response as a Java object
+        ChatPostMessageResponse response = null;
+        try {
+            response = methods.chatPostMessage(request);
 
-		// Check if the message sent successfully
-		if (response.isOk()) {
-			Message postedMessage = response.getMessage();
-			log.info("Success! Message posted to slack: {}", postedMessage);
-		} else {
-			String errorCode = response.getError(); // e.g., "invalid_auth", "channel_not_found"
-			log.error("Error while posting chat message in slack: {}", errorCode);
-		}
-	} catch (Exception e) {
-		log.error("Error while sending a message to Slack. @error={}", e.getMessage());
-	}
-}
+            // Check if the message sent successfully
+            if (response.isOk()) {
+                Message postedMessage = response.getMessage();
+                log.info("Success! Message posted to slack: {}", postedMessage);
+            } else {
+                String errorCode = response.getError(); // e.g., "invalid_auth", "channel_not_found"
+                log.error("Error while posting chat message in slack: {}", errorCode);
+            }
+        } catch (Exception e) {
+            log.error("Error while sending a message to Slack. @error={}", e.getMessage());
+        }
+    }
 }
